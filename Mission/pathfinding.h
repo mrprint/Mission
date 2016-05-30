426681654af09f38381a870a060b76626aa5d47c
mrprint <p2fedor@gmail.com>
2016-05-30 18:38:58 +0300

    Something missed at the previous stage

--- Mission/pathfinding.h
+++ Mission/pathfinding.h
@@ -157,7 +157,7 @@ protected:
     {
         get_path_ofs(path, start_p, finish_p);
         TCoords cp = start_p;
-        for (::vector<TCoords>::reverse_iterator it = path->rbegin(); it != path->rend(); ++it)
+        for (std::vector<TCoords>::reverse_iterator it = path->rbegin(); it != path->rend(); ++it)
         {
             cp.x += it->x;
             cp.y += it->y;
